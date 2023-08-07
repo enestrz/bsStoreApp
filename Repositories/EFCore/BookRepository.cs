8477484fa7b72fcfb8c144da6d789f69e6ae4115
Enes Terzi <enestrz15@gmail.com>
2023-08-07 14:44:45 +0300

    Add project files.

---
+++ Repositories/EFCore/BookRepository.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+
+namespace Repositories.EFCore
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(RepositoryContext context) : base(context)
+        {
+
+        }
+
+        public void CreateOneBook(Book book) => Create(book);
+
+        public void DeleteOneBook(Book book) => Delete(book);
+
+        public async Task<IEnumerable<Book>> GetAllBooksAsync(bool trackChanges) =>
+            await FindAll(trackChanges)
+            .OrderBy(b => b.Id)
+            .ToListAsync();
+
+        public async Task<Book> GetOneBookByIdAsync(int id, bool trackChanges) =>
+            await FindByCondition(b => b.Id.Equals(id), trackChanges)
+            .SingleOrDefaultAsync();
+
+        public void UpdateOneBook(Book book) => Update(book);
+    }
+}
---
+++ Repositories/EFCore/ReposityoryManager.cs
@@ -0,0 +1,29 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class ReposityoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+        private readonly Lazy<IBookRepository> _bookRepository;
+
+        public ReposityoryManager(RepositoryContext context)
+        {
+            _context = context;
+            _bookRepository = new Lazy<IBookRepository>(() => new BookRepository(_context));
+        }
+
+        public IBookRepository Book => _bookRepository.Value;
+
+        public async Task SaveAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
+
+    }
+}
