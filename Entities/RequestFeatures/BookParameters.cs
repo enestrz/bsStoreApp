bfd2f4b1db3a64ab41ae334738351861e07e33b0
Enes Terzi <enestrz15@gmail.com>
2023-08-13 21:41:34 +0300

    Filter implemented.

--- Entities/RequestFeatures/BookParameters.cs
+++ Entities/RequestFeatures/BookParameters.cs
@@ -2,6 +2,8 @@
 {
     public class BookParameters : RequestParameters
 	{
-
-	}
+        public uint MinPrice { get; set; }
+        public uint MaxPrice { get; set; } = 1000; // uint.MaxValue var
+        public bool ValidPriceRange => MaxPrice > MinPrice;
+    }
 }
---
+++ Repositories/EFCore/BookReposityoryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public static class BookReposityoryExtensions
+    {
+        public static IQueryable<Book> FilterBooks(this IQueryable<Book> books, 
+            uint minPrice, uint maxPrice) =>
+                books.Where(book => 
+                    (book.Price >= minPrice) && 
+                    (book.Price <= maxPrice));
+    }
+}
